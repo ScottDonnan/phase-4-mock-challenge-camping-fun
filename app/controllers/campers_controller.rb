df7e7b570f0b776fb0cc23eb74640428d24938a6
Scott Donnan <scott.donnan@me.com>
2021-11-08 11:19:42 -0600

    commit tree 1c9fa4cf8cf58b6867ff7369f91c3675e55b6fb7

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,30 @@
+class CampersController < ApplicationController
+
+    def index
+        render json: Camper.all
+    end
+
+    def show
+        camper = Camper.find_by(id: params[:id])
+        if camper
+            render json: camper, serializer: CamperDetailSerializer
+        else 
+            render json: {errors: "Camper not found"}, status: :not_found
+        end
+    end
+
+    def create
+        camper = Camper.create(camper_params)
+        if camper.valid?
+            render json: camper, status: :created
+        else
+            render json: {errors: camper.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def camper_params
+        params.permit :name, :age
+    end
+end
